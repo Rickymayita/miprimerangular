005737b34a135ed5a5750d4c25139d88709176a9
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-06-30 22:34:43 -0300

    primer componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimercomponenteComponent } from './primercomponente/primercomponente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimercomponenteComponent
   ],
   imports: [
     BrowserModule
